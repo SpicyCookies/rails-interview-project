79e8b467992f0e8e3414c91382ded1133e6cceb2
SpicyCookies <spicycookies8@gmail.com>
2020-10-12 20:50:00 -0500

    Modified comment for task tracking

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :question do
     title { FFaker::HipsterIpsum.sentence.gsub(/\.$/, '?') }
-    # BUG: The author of the main repository needs to
+    # BUG-1: The author of the main repository needs to
     #      not use 'private' as an attribute name, it's reserved.
     # rubocop:disable Layout/EmptyLinesAroundAccessModifier
     private { FFaker::Boolean.random }
