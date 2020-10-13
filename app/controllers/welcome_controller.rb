20c5b31032e78877d914eeb7eb69b78d2932b790
SpicyCookies <spicycookies8@gmail.com>
2020-10-13 18:03:34 -0500

    TASK-7 Add basic dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @total_users = User.count
+    @total_questions = Question.count
+    @total_answers = Answer.count
+    @total_tenants = Tenant.count
+    @tenants = Tenant.all
+  end
 end
