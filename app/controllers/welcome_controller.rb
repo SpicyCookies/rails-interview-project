859d323daf01c7f90d615cdcde9bcaa9db816e6a
SpicyCookies <57879598+SpicyCookies@users.noreply.github.com>
2020-10-14 21:13:16 -0500

    TASK-7 Add Basic Dashboard (#7)
    * TASK-7 Add basic dashboard
    
    * Removed unrequired total tenants card
    
    * Adjust scss for card

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @total_users = User.count
+    @total_questions = Question.count
+    @total_answers = Answer.count
+    @tenants = Tenant.all
+  end
 end
