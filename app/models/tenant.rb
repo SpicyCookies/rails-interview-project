291f293de604c4a2a3482d037d5c5b66cc66f670
SpicyCookies <spicycookies8@gmail.com>
2020-10-16 00:00:30 -0500

    TASK-10-Cleanup-code

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -3,11 +3,6 @@
 class Tenant < ApplicationRecord
   before_create :generate_api_key
 
-  validates :name,
-            presence: true
-  validates :request_count,
-            presence: true
-
   private
 
   def generate_api_key
